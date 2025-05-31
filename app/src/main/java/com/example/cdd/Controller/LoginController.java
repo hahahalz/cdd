60f2eb27c4d73a3db954a31cf5c54fa2f8ba96ab
Yuchenglu-1 <2842067680@qq.com>
2025-05-31 14:06:46 +0800

    Update LoginController.java

--- app/src/main/java/com/example/cdd/Controller/LoginController.java
+++ app/src/main/java/com/example/cdd/Controller/LoginController.java
@@ -1,5 +1,39 @@
 package com.example.cdd.Controller;
-
-public class LoginController {
+import com.example.cdd.View.LoginFragment;
+import com.example.cdd.Model.AuthManager;
+public class LoginController extends BaseController<LoginFragment,AuthManager > {
+    public LoginController() {
+        super();
+        model =new AuthManager();
+    }
+    public void initialize() {
+        view.onCreateView(null,null,null);
+    }
+    public void onDestroy() {
+        
+    }
+    public boolean loginresult(String userid, String password) {
+        this.model.login(userid,password);
+        if(this.model.getLoginResult())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+       
+    }
+    public boolean registerresult(String userid, String password) {
+        if(this.model.register(userid,password))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     // 负责登录/注册界面的逻辑
 }
+
